e16f4b9a23ba2b051327968d8540ae34ca2a9645
Raine Christine Perez <125470134+raynieee@users.noreply.github.com>
2024-05-20 05:09:10 +0800

    created functionality for methods in PointSET class

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # SE-2231-Lab-4
- 
+
+## Install dependencies
+```bash
+yarn install
+```
+
+## You can run with
+```bash
+npx parcel sample.html
+```
+
+```bash
+npx parcel rangesearch.html
+```
--- src/pointSET.ts
+++ src/pointSET.ts
@@ -2,13 +2,59 @@ import Point2D from "./doNotTouch/point2D";
 import RectHV from "./doNotTouch/rectHV";
 
 class PointSET {
-  public constructor() {} // construct an empty set of points
-  public isEmpty(): boolean {} // is the set empty?
-  public size(): number {} // number of points in the set
-  public insert(p: Point2D): void {} // add the point to the set (if it is not already in the set)
-  public contains(p: Point2D): boolean {} // does the set contain point p?
-  public draw(p): void {} // draw all points to p5
-  public range(rect: RectHV): Point2D[] {} // all points that are inside the rectangle (or on the boundary)
+  points: Point2D[];
+
+  public constructor() {
+    // construct an empty set of points
+    this.points = [];
+  }
+
+  public isEmpty(): boolean {
+    // is the set empty?
+    return this.points.length === 0;
+  }
+
+  public size(): number {
+    // number of points in the set
+    return this.points.length;
+  }
+
+  public insert(p: Point2D): void {
+    // add the point to the set (if it is not already in the set)
+    if (!this.contains(p)) {
+      this.points.push(p);
+    }
+  }
+
+  public contains(p: Point2D): boolean {
+    // does the set contain point p?
+    for (const point of this.points) {
+      if (point.equals(p)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public draw(p): void {
+    // draw all points to p5
+    for (const point of this.points) {
+      p.stroke(0);
+      p.strokeWeight(5);
+      point.draw(p);
+    }
+  }
+
+  public range(rect: RectHV): Point2D[] {
+    // all points that are inside the rectangle (or on the boundary)
+    const pointsInRect: Point2D[] = [];
+    for (const point of this.points) {
+      if (rect.contains(point)) {
+        pointsInRect.push(point);
+      }
+    }
+    return pointsInRect;
+  } 
 }
 
 export default PointSET;
